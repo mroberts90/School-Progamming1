829f41bff43a510bb529feb613922fba6de3d6e0
FreedomFaighter <john@freemania.pro>
2023-08-26 01:19:24 -0400

    Create main.java
    Update README.md
    
    Update .gitignore
    
    Create build-jar.yml
    
    Create pom.xml
    
    Updated main.java and pom.xml
    
    ADDED UPDATE TO build
    
    added Duration static method of creating the class
    Added string replace method for usage instead of string concat overload of +
    
    Update main.java
    
    updated string with final modifier
    
    Update README.md

---
+++ main.java
@@ -0,0 +1,46 @@
+package SCHOOLPROGRAMMING1;
+
+import java.time.Duration;
+import java.util.Scanner;
+
+public class Chapter2PPMatthewRoberts {
+
+	public static void main(String[] args) {
+
+		String name, college, petName, phrase;
+		int age, days, totalSeconds;
+
+		final int WEEK = 7;
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("What is your full name: ");
+		name = scan.nextLine();// whatever string the user inputs will be used in the name variable.
+
+		System.out.println("What is your age: ");
+		age = scan.nextInt();// the number that the user inputs for age will be used in the age variable.
+
+		System.out.println("How many days have you been away from your pet? ");
+		days = scan.nextInt();// the number that the user inputs for days away from pet will be used in the
+								// days variable.
+		scan.nextLine();
+
+		System.out.println("What college do you attend: ");
+		college = scan.nextLine();// the user's input for college will be used in the college variable.
+
+		System.out.println("What is your pet name: ");
+		petName = scan.nextLine();// temporarily stores the pets name entered into the petName variable.
+
+		System.out.println("What do you say to your pet every day when you are with him/her? ");
+		phrase = scan.nextLine();// temporarily stores what is typed for talking to pet
+		
+		System.out.println("Hello, my name is # and I am age ! years old.".replace('#', name).replace("!", String.valueOf(age)));
+		System.out.println("I'm enjoying my time at " + college + ", though I miss my pet " + petName + " very much!");
+		System.out.println("I have been away from " + petName + " for " + days + ".");
+		System.out.println("And that is an unbelievable " + Duration.ofDays(days).toSeconds() + " seconds.");
+		System.out.println("I have missed saying " + phrase + " for " + days + " days.");
+		final String message = "Even if I say it every week, not everyday,";
+		System.out.println(message);
+		System.out.println("I would still have missed saying " + phrase + " for " + (double) days / WEEK + " times.");
+	}
+}
